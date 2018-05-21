8ca3bbe96f4eed9ac0259178de414cf7eec2ae20
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-21 12:27:58 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,6 +1,7 @@
 p6df::modules::go::version() { echo "0.0.1" }
 p6df::modules::go::deps()    { 
-	ModuleDeps=()
+	ModuleDeps=(
+	)
 }
 
 p6df::modules::go::external::brew() {
@@ -10,6 +11,24 @@ p6df::modules::go::external::brew() {
 
 p6df::modules::go::init() {
 
+  p6df::modules::go::goenv::init
+}
+
+p6df::modules::go::goenv::init() {
+    [ -n "$DISABLE_ENVS" ] && return
+
+    export GOENV_ROOT=/Users/pgollucci/.local/share/wfarr/goenv
+    p6dfz::util::path_if $GOENV_ROOT/bin
+
+    if [ -x $GOENV_ROOT/bin/goenv ]; then
+      export HAS_GOENV=1
+      eval "$(goenv init - zsh)"
+    fi
+}
+
+p6df::prompt::go::line() {
+
+  env_version "go"
 }
 
 p6df::modules::go::init
