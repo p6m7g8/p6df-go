bd994b973457bad7724ee6340a1c7427b33d78e0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-11-14 08:16:35 -0500

    feat(deps): pulls the golang deps GitHub Actions uses (#12)

--- init.zsh
+++ init.zsh
@@ -48,14 +48,35 @@ p6df::modules::go::langs() {
   goenv global $latest
   goenv rehash
 
-  go get -u golang.org/x/tools/cmd/oracle
-  go get -u golang.org/x/tools/cmd/goimports
-  go get -u golang.org/x/tools/cmd/godoc
+  go get -u -v github.com/mdempsky/gocode 
+  go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs 
+  go get -u -v github.com/ramya-rao-a/go-outline 
+  go get -u -v github.com/acroca/go-symbols 
+  go get -u -v golang.org/x/tools/cmd/guru 
+  go get -u -v golang.org/x/tools/cmd/gorename 
+  go get -u -v github.com/fatih/gomodifytags 
+  go get -u -v github.com/haya14busa/goplay/cmd/goplay 
+  go get -u -v github.com/josharian/impl 
+  go get -u -v github.com/tylerb/gotype-live 
+  go get -u -v github.com/rogpeppe/godef 
+  go get -u -v github.com/zmb3/gogetdoc 
+  go get -u -v golang.org/x/tools/cmd/goimports 
+  go get -u -v github.com/sqs/goreturns 
+  go get -u -v winterdrache.de/goformat/goformat 
+  go get -u -v golang.org/x/lint/golint 
+  go get -u -v github.com/cweill/gotests/... 
+  go get -u -v github.com/alecthomas/gometalinter 
+  go get -u -v honnef.co/go/tools/... 
+  go get -u -v github.com/mgechev/revive 
+  go get -u -v github.com/sourcegraph/go-langserver 
+  go get -u -v github.com/go-delve/delve/cmd/dlv 
+  go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct 
+  go get -u -v github.com/godoctor/godoctor
 
-  go get -u github.com/stretchr/testify
-  go get -u github.com/golang/lint/golint
-
-  go get -u gopkg.in/check.v1
+  go get -u -v golang.org/x/tools/cmd/godoc
+  go get -u -v github.com/stretchr/testify
+  go get -u -v github.com/golang/lint/golint
+  go get -u -v gopkg.in/check.v1
 
   go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
   goenv rehash
