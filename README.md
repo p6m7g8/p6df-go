bce342e84a6b8f84b45ef2da7b0e02daf7b514f5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-06 13:17:14 -0500

    feat(api): p6df::prompt::gopath::line -> p6df::modules::go::goenv::prompt::line

--- README.md
+++ README.md
@@ -43,11 +43,11 @@
 - p6_go_prompt_info()
 - p6df::modules::go::deps()
 - p6df::modules::go::goenv::init(dir)
+- p6df::modules::go::goenv::prompt::line()
 - p6df::modules::go::home::symlink()
 - p6df::modules::go::init()
 - p6df::modules::go::langs()
 - p6df::modules::go::prompt::line()
-- p6df::prompt::gopath::line()
 - str str = p6_go_path_prompt_info()
 
 
--- init.zsh
+++ init.zsh
@@ -1,4 +1,3 @@
-
 ######################################################################
 #<
 #
@@ -23,7 +22,7 @@ p6df::modules::go::deps() {
 p6df::modules::go::home::symlink() {
 
   if [ -n "$GOPATH" ]; then
-    ln -fs $P6_DFZ_SRC_DIR $GOPATH/src 
+    ln -fs $P6_DFZ_SRC_DIR $GOPATH/src
   fi
 }
 
@@ -36,7 +35,10 @@ p6df::modules::go::home::symlink() {
 ######################################################################
 p6df::modules::go::langs() {
 
-  (cd $P6_DFZ_SRC_DIR/syndbg/goenv ; git pull)
+  (
+    cd $P6_DFZ_SRC_DIR/syndbg/goenv
+    git pull
+  )
 
   # nuke the old one
   local previous=$(goenv install -l | tail -2 | head -1 | sed -e 's, *,,g')
@@ -48,29 +50,29 @@ p6df::modules::go::langs() {
   goenv global $latest
   goenv rehash
 
-  go get -u -v github.com/mdempsky/gocode 
-  go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs 
-  go get -u -v github.com/ramya-rao-a/go-outline 
-  go get -u -v github.com/acroca/go-symbols 
-  go get -u -v golang.org/x/tools/cmd/guru 
-  go get -u -v golang.org/x/tools/cmd/gorename 
-  go get -u -v github.com/fatih/gomodifytags 
-  go get -u -v github.com/haya14busa/goplay/cmd/goplay 
-  go get -u -v github.com/josharian/impl 
-  go get -u -v github.com/tylerb/gotype-live 
-  go get -u -v github.com/rogpeppe/godef 
-  go get -u -v github.com/zmb3/gogetdoc 
-  go get -u -v golang.org/x/tools/cmd/goimports 
-  go get -u -v github.com/sqs/goreturns 
-  go get -u -v winterdrache.de/goformat/goformat 
-  go get -u -v golang.org/x/lint/golint 
-  go get -u -v github.com/cweill/gotests/... 
-  go get -u -v github.com/alecthomas/gometalinter 
-  go get -u -v honnef.co/go/tools/... 
-  go get -u -v github.com/mgechev/revive 
-  go get -u -v github.com/sourcegraph/go-langserver 
-  go get -u -v github.com/go-delve/delve/cmd/dlv 
-  go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct 
+  go get -u -v github.com/mdempsky/gocode
+  go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+  go get -u -v github.com/ramya-rao-a/go-outline
+  go get -u -v github.com/acroca/go-symbols
+  go get -u -v golang.org/x/tools/cmd/guru
+  go get -u -v golang.org/x/tools/cmd/gorename
+  go get -u -v github.com/fatih/gomodifytags
+  go get -u -v github.com/haya14busa/goplay/cmd/goplay
+  go get -u -v github.com/josharian/impl
+  go get -u -v github.com/tylerb/gotype-live
+  go get -u -v github.com/rogpeppe/godef
+  go get -u -v github.com/zmb3/gogetdoc
+  go get -u -v golang.org/x/tools/cmd/goimports
+  go get -u -v github.com/sqs/goreturns
+  go get -u -v winterdrache.de/goformat/goformat
+  go get -u -v golang.org/x/lint/golint
+  go get -u -v github.com/cweill/gotests/...
+  go get -u -v github.com/alecthomas/gometalinter
+  go get -u -v honnef.co/go/tools/...
+  go get -u -v github.com/mgechev/revive
+  go get -u -v github.com/sourcegraph/go-langserver
+  go get -u -v github.com/go-delve/delve/cmd/dlv
+  go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
   go get -u -v github.com/godoctor/godoctor
 
   go get -u -v golang.org/x/tools/cmd/godoc
@@ -81,7 +83,7 @@ p6df::modules::go::langs() {
   go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
   goenv rehash
   (
-    cd $P6_DFZ_SRC_DIR/github.com/golangci/golangci-lint/cmd/golangci-lint ;
+    cd $P6_DFZ_SRC_DIR/github.com/golangci/golangci-lint/cmd/golangci-lint
     go install -ldflags "-X 'main.version=$(git describe --tags)' -X 'main.commit=$(git rev-parse --short HEAD)' -X 'main.date=$(date)'"
   )
   goenv rehash
@@ -129,11 +131,11 @@ p6df::modules::go::goenv::init() {
 ######################################################################
 #<
 #
-# Function: p6df::prompt::gopath::line()
+# Function: p6df::modules::go::goenv::prompt::line()
 #
 #>
 ######################################################################
-p6df::prompt::gopath::line() {
+p6df::modules::go::goenv::prompt::line() {
 
   p6_go_path_prompt_info
 }
@@ -165,8 +167,8 @@ p6_go_path_prompt_info() {
 
   local str=
   if ! p6_string_blank "$GOPATH"; then
-    str="gopath:\t  $GOPATH
-goroot:\t  $GOROOT"
+    str="goenv:\t  gopath:$GOPATH
+goenv:\t  goroot:$GOROOT"
   fi
 
   p6_return_str "$str"
